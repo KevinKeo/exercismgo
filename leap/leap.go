ff7e50d8f659bf88d6b7d711348e8aa3068efe4c
icyks <keovilay.k@hotmail.fr>
2017-01-10 22:37:58 +0100

    solutionleap

--- leap/leap.go
+++ leap/leap.go
@@ -1,12 +1,21 @@
-// Leap stub file
-
-// The package name is expected by the test program.
+/*
+Leap package implements the solution for the leap exercice in excercism.io
+*/
 package leap
 
 // testVersion should match the targetTestVersion in the test file.
 const testVersion = 2
 
-// It's good style to write a comment here documenting IsLeapYear.
-func IsLeapYear(int) bool {
-	// Write some code here to pass the test suite.
+//IsLeapYear return true if the year is a leap
+func IsLeapYear(year int) bool {
+	if year%400 == 0 {
+		return true
+	}
+	if year%100 == 0 {
+		return false
+	}
+	if year%4 == 0 {
+		return true
+	}
+	return false
 }
