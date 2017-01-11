4f8b0a323baf30e9567dca31501a484b09175d0b
icyks <keovilay.k@hotmail.fr>
2017-01-11 19:54:07 +0100

    small correction comment

--- leap/leap.go
+++ leap/leap.go
@@ -1,12 +1,12 @@
 /*
-Leap package implements the solution for the leap exercice in excercism.io
+Package leap implements the solution for the leap exercice in excercism.io.
 */
 package leap
 
 // testVersion should match the targetTestVersion in the test file.
 const testVersion = 2
 
-//IsLeapYear return true if the year is a leap
+//IsLeapYear return true if the year is a leap.
 func IsLeapYear(year int) bool {
 	if year%400 == 0 {
 		return true
